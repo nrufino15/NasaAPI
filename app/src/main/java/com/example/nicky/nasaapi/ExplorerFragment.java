aa1f64dd0fe9a0ccc5769373159432b91b76668d
nrufino15 <nrufino@elpuig.xeill.net>
2019-02-09 03:03:42 +0100

    commit

--- app/src/main/java/com/example/nicky/nasaapi/ExplorerFragment.java
+++ app/src/main/java/com/example/nicky/nasaapi/ExplorerFragment.java
@@ -6,13 +6,9 @@ import android.support.v4.app.Fragment;
 
 import java.util.List;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class ExplorerFragment extends Fragment, SunListFragment {
+public class ExplorerFragment extends SunListFragment {
     @Override
     LiveData<List<Sun>> getSuns() {
-        return sunViewModel.getSuns()
+        return sunViewModel.getSuns();
     }
 }
